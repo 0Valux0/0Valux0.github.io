eca19c6b7fd78c153df9bc2a4b2893cc9b4f311a
Valux <valux.sh0p123@gmail.com>
2024-11-26 17:39:09 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,13 +1,9 @@
-const menuButton = document.getElementById('menu-button');
-const closeButton = document.getElementById('close-button');
-const menu = document.getElementById('menu');
+// Open the side menu
+function openMenu() {
+  document.getElementById("sideMenu").style.right = "0";
+}
 
-menuButton.addEventListener('click', () => {
-    menu.style.transition = 'transform 0.5s ease';
-    menu.style.transform = 'translateX(0)';
-});
-
-closeButton.addEventListener('click', () => {
-    menu.style.transition = 'transform 0.5s ease';
-    menu.style.transform = 'translateX(-100%)';
-});
+// Close the side menu
+function closeMenu() {
+  document.getElementById("sideMenu").style.right = "-250px";
+}
