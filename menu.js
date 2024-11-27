a39392f4e7941ad810b1ac1f8887988f431bab3c
Valux <valux.sh0p123@gmail.com>
2024-11-26 20:54:57 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,16 +1,1 @@
-let menuVisible = false;
 
-function openMenu() {
-    const menu = document.getElementById('sidenav');
-    
-    if (menuVisible) {
-        // Close the menu
-        menu.style.right = '-250px'; // Move it back to the right
-    } else {
-        // Open the menu
-        menu.style.right = '0'; // Move it into view
-    }
-    
-    // Toggle the visibility state
-    menuVisible = !menuVisible;
-}
