15b93a946cab303d9907432cc98ef634f4f2bc6e
Valux <valux.sh0p123@gmail.com>
2024-11-26 20:53:21 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,9 +1,16 @@
-// Open the side menu
-function openMenu() {
-  document.getElementById("sideMenu").style.right = "0";
-}
+let menuVisible = false;
 
-// Close the side menu
-function closeMenu() {
-  document.getElementById("sideMenu").style.right = "-250px";
+function openMenu() {
+    const menu = document.getElementById('sidenav');
+    
+    if (menuVisible) {
+        // Close the menu
+        menu.style.right = '-250px'; // Move it back to the right
+    } else {
+        // Open the menu
+        menu.style.right = '0'; // Move it into view
+    }
+    
+    // Toggle the visibility state
+    menuVisible = !menuVisible;
 }
